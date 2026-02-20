dd8252291aee38310e14942b36465d45d20841a2
Ulrik-Matemu <advancechronicles@gmail.com>
2026-02-20 18:06:03 +0300

    adjusted h2 in welcome note component fot better mobile responsiveness

--- src/app/components/welcome-note.tsx
+++ src/app/components/welcome-note.tsx
@@ -3,7 +3,7 @@ export const WelcomeNote = () => {
         <>
             <div className="bg-gray-200 md:py-12 py-8 ">
                 <h2 className="text-center px-8 md:px-20 md:pb-4 text-4xl md:text-5xl font-extrabold">
-                    Welcome to Silverstar Resources Co. Ltd.
+                    Welcome to Silverstar Resources Company Limited.
                 </h2>
                 <p className="text-center py-4 px-8  md:px-20 md:pt-0 text-xl">
                     SilverStar Resources Company Limited is among the leading supplier of superior quality construction materials and technical solutions in Tanzania such as ceramic and stone, installation products, surface protection, textiles materials and sport flooring.
