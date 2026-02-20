39ff88d51efdcc035bbd65219f6e0e5b29183305
Ulrik-Matemu <advancechronicles@gmail.com>
2026-02-20 17:58:35 +0300

    adjusted h2 in welcome note component fot better mobile responsiveness

--- src/app/components/welcome-note.tsx
+++ src/app/components/welcome-note.tsx
@@ -2,7 +2,7 @@ export const WelcomeNote = () => {
     return (
         <>
             <div className="bg-gray-200 md:py-12 py-8 ">
-                <h2 className="text-center px-8 md:px-20 md:pb-4 text-5xl font-extrabold">
+                <h2 className="text-center px-8 md:px-20 md:pb-4 text-4xl md:text-5xl font-extrabold">
                     Welcome to Silverstar Resources Co. Ltd.
                 </h2>
                 <p className="text-center py-4 px-8  md:px-20 md:pt-0 text-xl">
